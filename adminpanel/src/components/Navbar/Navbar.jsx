a2e9e2a80d1ab23abf7fb44ddd2be972414ed4ac
Burtarn <robin.andersson0125@gmail.com>
2025-04-01 15:33:12 +0200

    Fixed ikons

--- adminpanel/src/components/Navbar/Navbar.jsx
+++ adminpanel/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaHome, FaUsers, FaCog, FaChartBar, FaQuestionCircle, FaChevronDown } from 'react-icons/fa';
-import '../../components/Navbar/Navbar.css'
+import '../../components/Navbar/Navbar.css';
 
 const Sidebar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -23,11 +23,31 @@ const Sidebar = () => {
                 <button className="close-button" onClick={closeSidebar}>✖</button>
                 {isOpen && <h2 className="company-name">NeverAlone</h2>}
                 <ul>
-                    <li><Link to="/" onClick={closeSidebar}><FaHome />Home</Link></li>
-                    <li><Link to="/users" onClick={closeSidebar}><FaUsers /> Users</Link></li>
-                    <li><Link to="/settings" onClick={closeSidebar}><FaCog /> Settings</Link></li>
-                    <li><Link to="/reports" onClick={closeSidebar}><FaChartBar /> Reports</Link></li>
-                    <li><Link to="/help" onClick={closeSidebar}><FaQuestionCircle /> Help</Link></li>
+                    <li>
+                        <Link to="/" onClick={closeSidebar}>
+                            <FaHome /> {isOpen && 'Home'}
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/users" onClick={closeSidebar}>
+                            <FaUsers /> {isOpen && ' Users'}
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/settings" onClick={closeSidebar}>
+                            <FaCog /> {isOpen && ' Settings'}
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/reports" onClick={closeSidebar}>
+                            <FaChartBar /> {isOpen && ' Reports'}
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/help" onClick={closeSidebar}>
+                            <FaQuestionCircle /> {isOpen && ' Help'}
+                        </Link>
+                    </li>
                 </ul>
                 {!isOpen && (
                     <div className="toggle-arrow" onClick={toggleSidebar}>
