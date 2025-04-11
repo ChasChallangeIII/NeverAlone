6a8440face1674da7e40ebcafc60befb15b223fc
OzzoDev <oscar.burman005@gmail.com>
2025-04-11 10:13:53 +0200

    Created simple auth with signup/signin with in memory users array

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-import express from "express";
+import express, { json } from "express";
+import cookieParser from "cookie-parser";
 import { logging } from "./middleware/logging.js";
 import ApiRouter from "./routes/ApiRouter.js";
+import AuthRouter from "./routes/AuthRouter.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,11 +10,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("Welcome to the Express server of the NeverAlone project");
 });
 
 app.use("/api", logging, ApiRouter);
+app.use("/auth", AuthRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is very running at http://localhost:${PORT}`);
