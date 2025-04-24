184fc79a257433ccab823ee90863c7ec3196176b
Tobias Engvall <tobias.engvall@chasacademy.se>
2025-04-24 16:12:29 +0200

    errorHandler now handles ZodErrors, added .strict() to reportSchema

--- validations/reportSchema.js
+++ validations/reportSchema.js
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
-export const reportSchema = z.object({
-  time: z.string().min(1, "Time is required"),
-  location: z.string().min(1, "Location is required"),
-});
+export const reportSchema = z
+  .object({
+    time: z.string().min(1, "Time is required"),
+    location: z.string().min(1, "Location is required"),
+  })
+  .strict();
