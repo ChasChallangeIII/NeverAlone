132cf537fe381d7190cedd5f0878121a80271d22
OzzoDev <oscar.burman005@gmail.com>
2025-05-09 11:34:02 +0200

    Switched to bearer

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -2,7 +2,6 @@ import express from "express";
 import {
   removeAccount,
   signin,
-  signout,
   signup,
 } from "../controllers/AuthController.js";
 import { authenticate } from "../middleware/auth.js";
@@ -16,8 +15,6 @@ const router = express.Router();
 router.post("/signup", validateSignupReqBody, signup);
 
 router.post("/signin", validateSigninReqBody, signin);
-
-router.post("/signout", signout);
 
 router.delete("/account/:deletecommand", authenticate, removeAccount);
 
