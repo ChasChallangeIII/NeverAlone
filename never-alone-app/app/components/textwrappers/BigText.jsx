b6621c9d2aba1d3875a4e84cb2b3035bbaee3042
neriiman <neriman@live.se>
2025-05-11 01:52:17 +0200

    created text-wrappers

---
+++ never-alone-app/app/components/textwrappers/BigText.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { useTheme } from '../../context/ThemeContext'
+
+const BigText = ({children, style=null}) => {
+    const { customTheme } = useTheme()
+     const styles = createStyles(customTheme)
+     return (
+ 
+         <Text style={[styles.text, style]}>{children}</Text>
+ 
+     )
+ }
+ 
+export default BigText
+ 
+ const createStyles = (theme) => StyleSheet.create({
+     text: {
+         fontFamily: theme.fonts.semibold.fontFamily,
+         fontSize: 30
+     }
+ })
