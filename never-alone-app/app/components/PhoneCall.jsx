6c48b969e957621fe362c0f22c47a4d0ed64d526
neriiman <neriman@live.se>
2025-05-13 01:18:30 +0200

    updated call screen

--- never-alone-app/app/components/PhoneCall.jsx
+++ never-alone-app/app/components/PhoneCall.jsx
@@ -3,8 +3,12 @@ import React, { useEffect, useRef, useState } from 'react'
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useTheme } from '../context/ThemeContext';
 import MyText from './textwrappers/MyText';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Octicons from '@expo/vector-icons/Octicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
-
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 const PhoneCall = ({ visible, onClose }) => {
     const [elapsedTime, setElapsedTime] = useState(0)
@@ -54,27 +58,51 @@ const PhoneCall = ({ visible, onClose }) => {
         >
             <View
                 style={styles.modal}>
-                <Image
-                    style={styles.image}
-
-                    source={require('../assets/images/person.jpg')} />
-                <MyText style={styles.text}>Bestie<MaterialIcons name="heart-broken" size={24} color={"hotpink"} /></MyText>
-
-
-                <View>
-                    <MyText style={styles.text}>{formatTime()}</MyText>
+                <View style={styles.container1}>
+                    <Image
+                        style={styles.image}
+                        source={require('../assets/images/image.png')} />
+                    <MyText
+                        style={[styles.text, styles.name]}>
+                        hubby
+                    </MyText>
+                    <View>
+                        <MyText style={styles.text}>{formatTime()}</MyText>
+                    </View>
+                </View>
+                <View style={styles.container2}>
+                    <FontAwesome6
+                        name="microphone-slash"
+                        style={[styles.text, styles.phoneButtons]}
+                        size={40} />
+                    <Ionicons
+                        name="keypad"
+                        size={40}
+                        style={[styles.text, styles.phoneButtons]} />
+                    <Octicons
+                        style={[styles.text, styles.phoneButtons]}
+                        name="unmute"
+                        size={40} />
+                    <View style={[styles.text, styles.phoneButtons, styles.secondRow]}>
+                        <MaterialIcons
+                            style={[styles.text, styles.phoneButtons, {alignSelf: 'flex-start'}]}
+                            name="add-call"
+                            size={40} />
+                        <FontAwesome
+                            style={[styles.text, styles.phoneButtons, { alignSelf: 'flex-start' }]}
+                            name="video-camera"
+                            size={40} />
+                    </View>
                 </View>
 
-                {/* <Pressable onPress={hangUp}
+
+
+                <Pressable onPress={hangUp}
 
                 >
-                    <MaterialIcons name='phone' size={24} color={'red'} />
-                    <MyText>hejdå</MyText>
-                </Pressable> */}
-                <Button
-                    onPress={hangUp}
-                    title='Lägg på'
-                />
+                    <MaterialCommunityIcons name="phone-hangup" size={40} color={"white"} style={[styles.text, styles.hangUpButton]} />
+
+                </Pressable>
 
             </View>
         </Modal>
@@ -90,7 +118,24 @@ const createStyles = (theme, isDark) => StyleSheet.create({
         backgroundColor: isDark ? theme.colors.background : theme.colors.background900,
         justifyContent: 'flex-start',
         alignItems: 'center',
-        paddingTop: "20%"
+        paddingTop: "20%",
+        gap: 90
+    },
+    container1: {
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        gap: 10
+    },
+
+    container2: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        width: 'auto',
+        justifyContent: 'center',
+        paddingVertical: 30,
+        paddingHorizontal: 30,
+        alignItems: 'center',
+        gap: 80
     },
     image: {
         width: 120,
@@ -100,5 +145,29 @@ const createStyles = (theme, isDark) => StyleSheet.create({
     },
     text: {
         color: isDark ? theme.colors.text : theme.colors.primary50
+    },
+    name: {
+        fontSize: 40
+    },
+    phoneButtons: {
+        size: 40
+
+    },
+    secondRow: {
+        gap: 80,
+        paddingHorizontal: 30,
+        flex: 1,
+        flexDirection: 'row'
+    },
+    hangUpButton: {
+        backgroundColor: 'red',
+        padding: 20,
+        // height: 60,
+        // width: 60,
+        borderRadius: 90,
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+
     }
 })
