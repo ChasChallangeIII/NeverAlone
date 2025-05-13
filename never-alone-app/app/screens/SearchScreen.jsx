47d5820186702d011bf7d46925bff23d08f2d3cf
neriiman <neriman@live.se>
2025-05-13 02:23:59 +0200

    searchbar..

--- never-alone-app/app/screens/SearchScreen.jsx
+++ never-alone-app/app/screens/SearchScreen.jsx
@@ -1,17 +1,22 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
 import { useTheme } from '../context/ThemeContext'
 import MyText from '../components/textwrappers/MyText'
 import BigText from '../components/textwrappers/BigText'
 
+
 const SearchScreen = () => {
   const { customTheme } = useTheme()
   const styles = createStyles(customTheme)
   return (
-    <SafeAreaView>
-    <View>
-      <BigText>Rimi</BigText>
-      <MyText style={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint minus ipsam nam harum inventore iusto commodi dolorum nisi dolorem rerum corporis, ea optio incidunt quia. Sed sunt fugiat voluptates eum deleniti, dolorem atque molestiae, arores earum accusantium similique commodi!</MyText>
+    <SafeAreaView style={styles.screen} >
+      <View style={styles.container}>
+      <BigText>Sök vänner</BigText>
+        <TextInput
+          style={styles.searchBar}
+          placeholder='sök..'
+          placeholderTextColor={customTheme.colors.secondary700}
+        />
       </View>
     </SafeAreaView>
   )
@@ -20,5 +25,27 @@ const SearchScreen = () => {
 export default SearchScreen
 
 const createStyles = (theme) => StyleSheet.create({
-  
+  screen: {
+    flex: 1,
+    backgroundColor: theme.colors.background50,
+  },
+  container: {
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap:20
+  },
+
+  searchBar: {
+    borderWidth: 1,
+    borderColor: theme.colors.secondary800,
+    color: theme.colors.secondary800,
+    height: 50,
+    fontSize: theme.fonts.regular.fontSize,
+    paddingHorizontal: 15,
+    backgroundColor: theme.colors.backgroundColor,
+    width: 300,
+    borderRadius:50
+
+  }
 })
