9d68151df78536cff3fe5e5cda72ed7e89ad0ca7
OzzoDev <oscar.burman005@gmail.com>
2025-05-16 10:12:03 +0200

    Added route for groups

--- routes/ApiRouter.js
+++ routes/ApiRouter.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { addReport } from "../controllers/ReportController.js";
 import { validateReportReqBody } from "../validators/reportValidator.js";
-import ContactRoutes from "./ContactRoutes.js";
+import ContactRouter from "./ContactRouter.js";
+import GroupRouter from "./GroupRouter.js";
 
 const router = express.Router();
 
 router.post("/reports", validateReportReqBody, addReport);
 
-router.use("/contacts", ContactRoutes);
+router.use("/contacts", ContactRouter);
+
+router.use("/groups", GroupRouter);
 
 export default router;
---
+++ routes/GroupRouter.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const router = express.Router();
+
+//create group
+// router.post("/")
+
+//join group
+// router.patch("/")
+
+//leave group
+// router.patch("/leave")
+
+//get groups
+// router.get("/")
+
+//search groups
+// router.get("/?q")
+
+export default router;
