6bcceba9f586eb34c96c483c972e6a49ab9ad855
DavidReyes <143813616+DavidJanReyes@users.noreply.github.com>
2025-05-19 11:27:13 +0200

    FakeCall on BLE signal - test

--- never-alone-app/App.js
+++ never-alone-app/App.js
@@ -1,16 +1,19 @@
 import { AuthProvider } from "./app/context/AuthContext";
 import { ThemeProvider } from "./app/context/ThemeContext";
 import { UserProvider } from "./app/context/UserContext";
+import { CallUIProvider } from "./app/context/CallUIContext";
 import AppNavigation from "./app/navigation/AppNavigation";
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <UserProvider>
-          <AppNavigation />
-        </UserProvider>
-      </AuthProvider>
-    </ThemeProvider>
+    <CallUIProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <UserProvider>
+            <AppNavigation />
+          </UserProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </CallUIProvider>
   );
 }
