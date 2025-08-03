19a5498ce91e60d2f9e9352add05d1149fdc480a
neriiman <neriman@live.se>
2025-08-03 23:00:52 +0200

    go back btn in postscreen

--- never-alone-app/app/screens/PostScreen.jsx
+++ never-alone-app/app/screens/PostScreen.jsx
@@ -1,49 +1,63 @@
-import { Image, Platform, StatusBar, StyleSheet, Text, View } from 'react-native'
+import { Image, Platform, Pressable, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
 import { formatDistanceToNow } from 'date-fns'
 import { sv } from 'date-fns/locale'
 import React from 'react'
 import MyText from '../components/textwrappers/MyText'
 import BigText from '../components/textwrappers/BigText'
 import { useTheme } from '../context/ThemeContext'
+import AntDesign from '@expo/vector-icons/AntDesign';
 
-const PostScreen = ({ route }) => {
+const PostScreen = ({ navigation, route }) => {
     const { item } = route.params
     const { customTheme, isDark } = useTheme()
     const styles = createStyles(customTheme, isDark)
+    const onClose = () => navigation.goBack()
+
 
     return (
-        <View style={styles.container}>
-            <View
-                style={styles.post}
-                accessible={true}
-                accessibilityLabel={`${item.username} skrev detta: "${item.text}"`}
-            >
-                <View style={
-                    styles.postDetailsContainer
-                }>
-                    <Image
-                        source={{ uri: item.profileImage }}
-                        style={styles.profileImage}
-                        accessibilityLabel={`${item.username}s profilbild `}
-                    />
-                    <View
-                        style={styles.postDetails}
-                    >
-                        <BigText
-                            style={{ fontSize: 16 }}
-                        >{item.username}</BigText>
-                        <MyText
-                            style={{ fontSize: 10 }}
-                        >{formatDistanceToNow(item.date, { addSuffix: true, locale: sv })}</MyText>
-                    </View>
-                </View>
-                <MyText
-                    style={styles.postText}
+        <SafeAreaView style={styles.screen}>
+           
+            <View style={styles.container}>
+                <View
+                    style={styles.post}
+                    accessible={true}
+                    accessibilityLabel={`${item.username} skrev detta: "${item.text}"`}
                 >
-                    {item.text}
-                </MyText>
+                    <View style={
+                        styles.postDetailsContainer
+                    }>
+                        <Image
+                            source={{ uri: item.profileImage }}
+                            style={styles.profileImage}
+                            accessibilityLabel={`${item.username}s profilbild `}
+                        />
+                        <View
+                            style={styles.postDetails}
+                        >
+                            <BigText
+                                style={{ fontSize: 16 }}
+                            >{item.username}</BigText>
+                            <MyText
+                                style={{ fontSize: 10 }}
+                            >{formatDistanceToNow(item.date, { addSuffix: true, locale: sv })}</MyText>
+                        </View>
+                    </View>
+                    <MyText
+                        style={styles.postText}
+                    >
+                        {item.text}
+                    </MyText>
+                </View>
+                <Pressable
+                    style={styles.close}
+                    onPress={onClose}
+                    accessibilityLabel='tryck här för att gå tillbaka till community-sidan'
+                    accessibilityRole='button'   
+                >
+                    <AntDesign style={styles.closeIcon} name='back' />
+                </Pressable>
             </View>
-        </View>
+        </SafeAreaView>
     )
 }
 
@@ -58,7 +72,7 @@ const createStyles = (theme, isDark) => StyleSheet.create({
     },
     container: {
         flex: 1,
-        justifyContent: 'center',
+        // justifyContent: 'center',
         alignItems: 'center',
         gap: 20,
         paddingHorizontal: 8,
@@ -93,5 +107,17 @@ const createStyles = (theme, isDark) => StyleSheet.create({
     postText: {
 
 
-    }
+    },
+    close: {
+        position: 'absolute',
+        right: 0,
+        // alignItems:'flex-end',
+        padding: 20,
+        color: theme.colors.text
+    },
+    closeIcon: {
+        color: theme.colors.text,
+        fontSize: 24
+
+    },
 })
